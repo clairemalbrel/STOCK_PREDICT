695beaa8a572c4e2cfe31ef659e008cd58874d28
Christophe-Arendt <christophe.arendt@edu.escpeurope.eu>
2020-06-29 19:24:19 +0200

    daily data python script

---
+++ STOCK_PREDICT/daily_data.py
@@ -0,0 +1,104 @@
+##########################################################################
+#     Stock Predict dail dataset
+##########################################################################
+
+import datetime as dt
+import json
+import requests
+import requests
+from datetime import datetime
+import time
+import pandas as pd
+import math
+
+
+##########################################################################
+#     Reddit scrap
+##########################################################################
+
+text = []
+name = []
+today = dt.datetime.combine(dt.date.today(), dt.datetime.min.time())
+response = requests.get('https://www.reddit.com/r/worldnews/top.json?limit=40', headers = {'User-agent': 'test'})
+tops = json.loads(response.text)['data']['children']
+i = 1
+for top in tops:
+    if dt.datetime.fromtimestamp(top['data']['created']) >= today:
+        text.append(top['data']['title'])
+        name.append(f'Top{i}')
+        i = i + 1
+
+#full = clean("".join(str(text)))
+data = pd.DataFrame(text).T
+data.columns = name
+data = data.iloc[:,0:25]
+data['Date'] = datetime.date(today)
+
+##########################################################################
+#     Yahoo Finance
+##########################################################################
+
+def date_time(x):
+    x = datetime.date(x)
+    return x
+
+# Get timestamp of today
+ts = int(time.time())
+
+# yaho API
+url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-historical-data"
+
+querystring = {"frequency":"1d","filter":"history","period1":"1592179200","period2":str(ts),"symbol":"%5EDJI"}
+
+headers = {
+    'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com",
+    'x-rapidapi-key': "9f58e76de3msh0050a18aab7452fp15c352jsn04ff99cb8ff8"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+re = response.json()
+
+# Bring response to dataframe format
+major_liste = []
+
+for i in re['prices']:
+    liste =[datetime.fromtimestamp(i['date']),
+            i['open'],
+            i['high'],
+            i['low'],
+            i['close'],
+            i['volume'],
+            i['adjclose']]
+    major_liste.append(liste)
+
+new = pd.DataFrame(major_liste)
+new.columns = ['Date','Open','High','Low','Close','Volume','Adj Close']
+new['Date']=new['Date'].apply(date_time)
+
+# sort and calculate change
+new =  new.sort_values('Date')
+new['change'] = new['Open'].pct_change()
+new['change'] = new['change'].shift(-1)
+
+def categorical(x):
+    if x >= 0:
+        x = 1
+    if x < 0:
+        x = 0
+    if math.isnan(x):
+        x = 'today'
+    return x
+
+new['target'] = new['change'].apply(categorical)
+
+##########################################################################
+#     Merge and add to csv
+##########################################################################
+
+y0 = data.merge(new, how='inner')
+y0.head()
+print(y0)
+#y0.to_csv('stock_predict_daily.csv')
+
+y0.to_csv('../data/daily_data.csv', mode='a', header=True)
+
--- STOCK_PREDICT/reddit_scrap.py
+++ STOCK_PREDICT/reddit_scrap.py
@@ -5,19 +5,24 @@
 import datetime as dt
 import json
 import requests
+import pandas as pd
 
 
-# Test modif
-text = []
-today = dt.datetime.combine(dt.date.today(), dt.datetime.min.time())
-response = requests.get('https://www.reddit.com/r/worldnews/top.json?limit=25', headers = {'User-agent': 'test'})
-tops = json.loads(response.text)['data']['children']
-for top in tops:
-  if dt.datetime.fromtimestamp(top['data']['created']) >= today:
-    text.append(top['data']['title'])
-  else:
-    text.append(top['data']['created'])
+def reddit():
+  text = []
+  name = []
+  today = dt.datetime.combine(dt.date.today(), dt.datetime.min.time())
+  response = requests.get('https://www.reddit.com/r/worldnews/top.json?limit=40', headers = {'User-agent': 'test'})
+  tops = json.loads(response.text)['data']['children']
+  i = 1
+  for top in tops:
+      if dt.datetime.fromtimestamp(top['data']['created']) >= today:
+          text.append(top['data']['title'])
+          name.append(f'Top{i}')
+          i = i + 1
 
-full = clean("".join(str(text)))
-new_df = pd.DataFrame(data, columns = ['Date', 'cleaned'])
-new_df
+  #full = clean("".join(str(text)))
+  data = pd.DataFrame(text).T
+  data.columns = name
+  data = data.iloc[:,0:25]
+  return data
